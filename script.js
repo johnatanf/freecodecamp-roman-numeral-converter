55f34b8ecc4d77cbbef596628f1a9dae26db3c6a
johnatanf <johnatan.f100@gmail.com>
2024-06-11 11:21:35 +0700

    create convertNumberToRomanNumeral function

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-const romanNumerals = [
-    {"M": 1000},
-    {"CM": 900},
-    {"D": 500},
-    {"CD": 400},
-    {"C": 100},
-    {"XC": 90},
-    {"L": 50},
-    {"XL": 40},
-    {"X": 10},
-    {"IX": 9},
-    {"V": 5},
-    {"IV": 4},
-    {"I": 1}
-]
-
 function validateNumber(num) {
     const divOutput = document.getElementById('output')
 
@@ -29,4 +13,47 @@ function validateNumber(num) {
     } else {
         return true;
     }
+}
+
+function convertNumberToRomanNumeral(num) {
+    const romanNumerals = [
+        {"M": 1000},
+        {"CM": 900},
+        {"D": 500},
+        {"CD": 400},
+        {"C": 100},
+        {"XC": 90},
+        {"L": 50},
+        {"XL": 40},
+        {"X": 10},
+        {"IX": 9},
+        {"V": 5},
+        {"IV": 4},
+        {"I": 1}
+    ]
+    
+    let romanNumeralString = ""
+    let remainder = num
+    let index = 0 //start from highest roman numeral, M: 1000
+    let multiple
+    let romanNumericValue
+    let romanLetter
+
+    while(remainder > 0) {
+        
+        romanNumericValue = Object.values(romanNumerals[index])[0]
+        romanLetter = Object.keys(romanNumerals[index])[0]
+
+        if(remainder / romanNumericValue >= 1) {
+            multiple = Math.floor(remainder / romanNumericValue)
+            romanNumeralString = romanNumeralString.concat(romanLetter.repeat(multiple))
+            remainder -= romanNumericValue*multiple
+        } else {
+            // skip
+        }
+
+        index++
+    }
+
+    return romanNumeralString
 }
